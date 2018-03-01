9183d20d7da686c3c83c3fd65321eac32022d0f3
Jullian Cockerell <jullian.cockerell@yahoo.com>
2018-03-01 01:19:24 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,48 @@
+function whileLoop(number)
+{
+  while(number >= 0)
+  {
+    console.log(number);
+    number = number - 1;
+  }
+  return 'done';
+}
+
+function forLoop(array)
+{
+  for(var i = 1; i < 26; i++)
+  {
+    if(i === 1)
+    {
+      array.push(`I am ${i} strange loop.`);
+    }
+    else
+    {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function doWhileLoop(array)
+{
+  do
+  {
+    array.pop();
+  } while(maybeTrue() && array.length > 0);
+}
+
+function maybeTrue() 
+{
+  return Math.random() >= 0.5
+}
+
+
+
+
+
+
+
+
+
+
